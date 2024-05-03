ed3fd786f085baf5b1c9af500bf71202ab73db5c
Marcos Moretto <marcosmoretto@Marcoss-MBP.fibertel.com.ar>
2024-05-02 22:19:37 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/logoribs.jpg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import Rating from "@material-ui/lab/Rating";
+import { TextareaAutosize } from "@material-ui/core";
 
 function App() {
+  const [rating, setRating] = useState(0);
+
+  useEffect(() => {
+    if (rating > 3) {
+      setTimeout(() => {
+        window.open(
+          "https://search.google.com/local/writereview?placeid=ChIJv53bi621vJUREPI-e0F1duo",
+          "_blank"
+        );
+      }, 300);
+    }
+  }, [rating]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        <h3>¿Como fue tu experiencia?</h3>
+
+        <Rating
+          name="pristine"
+          value={rating}
+          onChange={(event, newValue) => {
+            setRating(newValue);
+          }}
+          iconHover={{ color: "gold" }}
+          iconFilled={{ color: "gold" }}
+          size="large"
+        />
+
+        {rating <= 3 ? (
+          <TextareaAutosize placeholder="Dejanos un comentario" />
+        ) : null}
+      </body>
     </div>
   );
 }
