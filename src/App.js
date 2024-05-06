8e2f25a5c7dd6eadf70438af2f67b877cd2ff357
Marcos Moretto <marcosmoretto@Marcoss-MBP.fibertel.com.ar>
2024-05-05 22:21:43 -0300

    fix router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Reviewer from "./Reviewer";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/rib-reviews/:soho" element={<Reviewer />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="" element={<Reviewer />} />
+      </Routes>
     </div>
   );
 }
